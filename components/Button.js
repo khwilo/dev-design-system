76e0f6dc09be355eafdce4b1aa6d34970403a5e9
Khwilo Kabaka <khwilowatai@gmail.com>
2020-12-14 12:08:02 +0300

    Create the default button component

---
+++ components/Button.js
@@ -0,0 +1,3 @@
+const Button = ({ title }) => <button className='button'>{title}</button>;
+
+export default Button;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import Head from 'next/head';
+import Button from '../components/Button';
 
 export default function Home() {
   return (
-    <div>
+    <div className='wrapper'>
       <Head>
         <title>Dev Challenges | Design System</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+
+      <section className='section flow'>
+        <h1 className='section-heading1'>Buttons</h1>
+
+        <div className='section__btn flex'>
+          <div>
+            <h3 className='button-label'>{`<Button />`}</h3>
+            <Button title='Default' />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
