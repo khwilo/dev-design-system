844ba2dc0746539d16acc96f51f2f5859d763796
Khwilo Kabaka <khwilowatai@gmail.com>
2020-12-15 12:09:32 +0300

    Use a modified version of BEM

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,4 +1,5 @@
 import styles from './Button.module.css';
+import cn from 'classnames';
 
 const Button = ({
   title,
@@ -10,24 +11,40 @@ const Button = ({
   disabledShadow = false,
   disabled = false,
 }) => {
+  const computedClassNames = cn(styles.Button, {
+    [styles.Button___outline]: variant === 'outline',
+    [styles.Button___text]: variant === 'text',
+    [styles.Button___disabledShadow]: disabledShadow,
+    [styles.Button___disabled]: disabled,
+    [styles.Button___textDisabled]: variant === 'text' && disabled,
+    [styles.Button_iconTitleWrapper]: startIcon || endIcon,
+    [styles.Button___sm]: size === 'sm',
+    [styles.Button___md]: size === 'md',
+    [styles.Button___lg]: size === 'lg',
+    [styles.Button___primary]: color === 'primary',
+    [styles.Button___secondary]: color === 'secondary',
+    [styles.Button___danger]: color === 'danger',
+  });
+
   return (
-    <button
-      className={`${styles.button} ${variant ? styles[variant] : ''} ${
-        disabledShadow ? styles.disabledShadow : ''
-      } ${disabled ? styles.disabled : ''} ${
-        startIcon || endIcon ? styles.iconBtnWrapper : ''
-      } ${size ? styles[size] : ''} ${color ? styles[color] : ''}
-      `}
-    >
+    <button className={computedClassNames}>
       {startIcon ? (
-        <div className={styles.iconBtn}>
-          <img src='assets/images/add_shopping_cart.svg' alt='' />
+        <div className={styles.Button_iconTitle}>
+          <img
+            className={styles.Button_iconTitle___img}
+            src='assets/images/add_shopping_cart.svg'
+            alt=''
+          />
           <span>{title}</span>
         </div>
       ) : endIcon ? (
-        <div className={styles.iconBtn}>
+        <div className={styles.Button_iconTitle}>
           <span>{title}</span>
-          <img src='assets/images/add_shopping_cart.svg' alt='' />
+          <img
+            className={styles.Button_iconTitle___img}
+            src='assets/images/add_shopping_cart.svg'
+            alt=''
+          />
         </div>
       ) : (
         <>{title}</>
